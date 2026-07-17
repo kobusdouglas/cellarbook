cbfa0ac45a3f515ed9cc5b9fd656170a35c37e11
Kobus Douglas <79973280+kobusdouglas@users.noreply.github.com>
2026-07-17 12:42:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cellarbook-v40';
+const CACHE = 'cellarbook-v41';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
